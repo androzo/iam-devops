5f3274f97f99c22068f8d67182a92a17829b118f
Andre Buchmann <androzo@gmail.com>
2025-04-05 18:08:47 -0300

    fix bucket name

--- teams/team-anders/devl/deployment-role/backend.tf
+++ teams/team-anders/devl/deployment-role/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "androzo-terraform-state" # Replace with your S3 bucket name
+    bucket         = "androzo-terraform-tfstate" # Replace with your S3 bucket name
     key            = "devl/team-anders/deployment-role/terraform.tfstate" # Path to the state file
     region         = "sa-east-1"
     encrypt        = true              
--- teams/team-drozer/devl/deployment-role/backend.tf
+++ teams/team-drozer/devl/deployment-role/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "androzo-terraform-state" # Replace with your S3 bucket name
+    bucket         = "androzo-terraform-tfstate" # Replace with your S3 bucket name
     key            = "devl/${var.team_name}/deployment-role/terraform.tfstate" # Path to the state file
     region         = var.aws_region
     encrypt        = true              
