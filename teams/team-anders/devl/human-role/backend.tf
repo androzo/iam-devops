588004d73a783c9b83994ded3343c47db965cba1
Andre Buchmann <androzo@gmail.com>
2025-04-05 18:06:38 -0300

    fix variable

--- teams/team-anders/devl/human-role/backend.tf
+++ teams/team-anders/devl/human-role/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "androzo-terraform-state" # Replace with your S3 bucket name
-    key            = "devl/${var.team_name}/human-role/terraform.tfstate" # Path to the state file
-    region         = var.aws_region
+    key            = "devl/team-anders/human-role/terraform.tfstate" # Path to the state file
+    region         = "sa-east-1"
     encrypt        = true              
   }
 }
