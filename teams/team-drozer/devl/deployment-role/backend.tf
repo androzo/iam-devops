7507bd001e0a491a4fc52a18d851f231cc5dfaf9
André Buchmann <androzo@gmail.com>
2025-04-12 16:37:08 -0300

    fix:  backend

--- teams/team-drozer/devl/deployment-role/backend.tf
+++ teams/team-drozer/devl/deployment-role/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket               = "androzo-terraform-tfstate" # Replace with your S3 bucket name
     key                  = "deployment-role.tfstate"   # Path to the state file
-    workspace_key_prefix = "team-drozer/devl/"         # Prefix for workspaces
+    workspace_key_prefix = "team-drozer"               # Prefix for workspaces
     region               = "sa-east-1"
     encrypt              = true
   }
