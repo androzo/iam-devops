7a2772631fbec5ca98dcf9900d7665e325b0b185
André Buchmann <androzo@gmail.com>
2025-04-12 08:56:36 -0300

    change statefile

--- teams/team-drozer/devl/deployment-role/backend.tf
+++ teams/team-drozer/devl/deployment-role/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "androzo-terraform-tfstate"                          # Replace with your S3 bucket name
-    key     = "devl/team-drozer/deployment-role/terraform.tfstate" # Path to the state file
+    bucket  = "androzo-terraform-tfstate"                     # Replace with your S3 bucket name
+    key     = "team-drozer/deployment-role/terraform.tfstate" # Path to the state file
     region  = "sa-east-1"
     encrypt = true
   }
